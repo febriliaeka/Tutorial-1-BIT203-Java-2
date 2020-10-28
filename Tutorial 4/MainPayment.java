35628af9ad8d55c03e18478cf38d9de771871911
febriliaeka <61887999+febriliaeka@users.noreply.github.com>
2020-10-28 11:37:36 +0800

    Update MainPayment.java

--- Tutorial 4/MainPayment.java
+++ Tutorial 4/MainPayment.java
@@ -9,6 +9,48 @@ package tutor;
  *
  * @author ASUS
  */
+
+import java.util.*;
+
 public class MainPayment {
-    
+    public static void main(String[] args){
+        Scanner key = new Scanner(System.in);
+
+        boolean ext = false;
+
+        while(!ext){
+            System.out.print("Sales amount? (0 to exit) ");
+            double amount = key.nextDouble();
+            key.nextLine();
+
+            if(amount != 0)
+            {
+                System.out.print("Payment type? <Cash> or <Credit> ");
+                String pay = key.nextLine();
+
+                if(pay.equalsIgnoreCase("cash")){
+                    System.out.println("\n" + new CashPayment(amount) +"\n");
+                }
+                else if (pay.equalsIgnoreCase("credit"))
+                {
+                    System.out.print("Credit Card Holder: ");
+                    String nameCard = key.nextLine();
+                    System.out.print("Expire on: (mm/yy) ");
+                    String date = key.nextLine();
+                    System.out.print("Credit Card Number: ");
+                    String numCard = key.nextLine();
+                    
+                    System.out.println(new CreditCardPayment(amount, nameCard, date, numCard) + "\n");
+                }
+                else
+                    System.out.println("Incorrect Payment Type! Enter <Cash> or <Credit>\n");
+            }
+            else
+                    ext = true;
+
+        }
+
+        System.out.println("\nThank u!");
+        key.close();
+    }
 }
